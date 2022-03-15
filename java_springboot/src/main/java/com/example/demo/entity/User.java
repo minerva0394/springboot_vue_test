98ab517a3d5c457c2166ce05a20a07a3ea89ec5a
曾智麟 <>
2022-03-16 03:21:06 +0800

    后台基本框架+前台方法 2022-03-16-03:21

---
+++ java_springboot/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@TableName("user")
+@Data
+public class User {
+//    主键不为id时，要写value
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private String username;
+    private String password;
+    private Integer age;
+    private String sex;
+    private String address;
+}
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div style="padding: 10px">
     <!--    功能区-->
     <div style="margin: 10px 0">
-      <el-button type="primary">新增</el-button>
+      <el-button type="primary" @click="add">新增</el-button>
       <el-button type="primary">导入</el-button>
       <el-button type="primary">导出</el-button>
     </div>
@@ -18,15 +18,27 @@
         stripe
         style="width: 100%;">
       <el-table-column
-          prop="date"
-          label="日期"
+          prop="id"
+          label="ID"
           sortable
       >
       </el-table-column>
       <el-table-column
-          prop="name"
-          label="姓名"
+          prop="userName"
+          label="用户名"
       >
+      </el-table-column>
+      <el-table-column
+          prop="nickName"
+          label="昵称">
+      </el-table-column>
+      <el-table-column
+          prop="age"
+          label="年龄">
+      </el-table-column>
+      <el-table-column
+          prop="sex"
+          label="性别">
       </el-table-column>
       <el-table-column
           prop="address"
@@ -58,10 +70,36 @@
             layout="total, sizes, prev, pager, next, jumper"
             :total="totalPage">
         </el-pagination>
+
+        <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+          <el-form :model="form" label-width="120px">
+            <el-form-item label="用户名:">
+              <el-input v-model="form.username" style="width: 80%"></el-input>
+            </el-form-item>
+            <el-form-item label="昵称:">
+              <el-input v-model="form.nickName" style="width: 80%"></el-input>
+            </el-form-item>
+            <el-form-item label="年龄:">
+              <el-input v-model="form.age" style="width: 80%"></el-input>
+            </el-form-item>
+            <el-form-item label="性别:">
+              <el-radio v-model="form.sex" label="1" style="width: 80%">男</el-radio>
+              <el-radio v-model="form.sex" label="2" style="width: 80%">女</el-radio>
+              <el-radio v-model="form.sex" label="3" style="width: 80%">未知</el-radio>
+            </el-form-item>
+            <el-form-item label="地址:">
+              <el-input type="textarea" v-model="form.address" style="width: 80%"></el-input>
+            </el-form-item>
+          </el-form>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="save">确 定</el-button>
+          </span>
+        </el-dialog>
+
       </div>
 
     </div>
-
   </div>
 </template>
 
@@ -71,28 +109,21 @@ export default {
   components: {},
   data() {
     return {
+      form:{},
+      dialogVisible:false,
       search: [],
       totalPage:30,
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      tableData: []
     }
   },
   methods: {
+    add(){
+      this.dialogVisible = true
+      this.form = {}
+    },
+    save(){
+      
+    },
     handleEdit() {
 
     },
